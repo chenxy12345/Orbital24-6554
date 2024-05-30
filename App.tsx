af6def403b8434e22168d0beb0ac6933efcc5478
darrny <167701231+darrny@users.noreply.github.com>
2024-05-30 17:06:47 +0800

    30052024 1707

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+
 
 export default function App() {
   return (
