fd58d27a2c3a946827d7ea284f79152a4e57739a
chenxy12345 <156636799+chenxy12345@users.noreply.github.com>
2024-05-30 16:50:30 +0800

    Add files via upload

---
+++ app/screens/SpotsScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SpotsScreen = () => {
+  return (
+    <View>
+      <Text>SpotsScreen</Text>
+    </View>
+  )
+}
+
+export default SpotsScreen
+
+const styles = StyleSheet.create({})
